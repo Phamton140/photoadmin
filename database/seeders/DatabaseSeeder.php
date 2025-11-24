474cc355fcfd690e8b81a0f8d2d18f124e0a44cd
Phamton140 <genesismsf@gmail.com>
2025-11-23 21:14:49 -0400

    Se resolvieron algunos problemas con los seeders del lado del servidor

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         // Ejecutar seeders principales
         $this->call([
             PermissionSeeder::class,
+            RolePermissionSeeder::class,
             SuperAdminSeeder::class,
         ]);
     }
